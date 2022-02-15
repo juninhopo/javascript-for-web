b5fd01d899066ddae3c44695737af849c3ab8a16
juninhopo <kelgaxd@gmail.com>
2022-02-15 01:01:07 -0300

    update

---
+++ js/principal.js
@@ -0,0 +1,14 @@
+let titulo = document.querySelector(".titulo")
+titulo.textContent = 'Clientes'
+
+let paciente = document.querySelector("#primeiro-paciente")
+
+let tdPeso = paciente.querySelector(".info-peso")
+peso = tdPeso.textContent
+
+let tdAltura = paciente.querySelector(".info-altura")
+let altura = tdAltura.textContent
+
+console.log(paciente)
+console.log(tdAltura)
+console.log(altura)
